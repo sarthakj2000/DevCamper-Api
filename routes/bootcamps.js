2b3ec233c2072e0ef3660f4cdcc2f32dc1dd57f5
sarthak <sarthakjaiswal45@gmail.com>
2020-07-14 12:57:02 +0530

    create advanced results for pagening stuffs

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,8 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 //Include other course routers
 const courseRouter = require('./courses');
@@ -19,7 +21,10 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload);
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
